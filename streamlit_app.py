85fd8fb25ea4f30a241721674ee46a977d0629f6
dineshrajsr08 <75078446+dineshrajsr08@users.noreply.github.com>
2024-01-20 14:10:26 -0600

    Update streamlit_app.py
    Updated Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 streamlit.title('My Parents Healthy New Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Muffin')
-streamlit.text('Kale.Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 and Blueberry Muffin')
+streamlit.text('🥗Kale.Spinach and Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
